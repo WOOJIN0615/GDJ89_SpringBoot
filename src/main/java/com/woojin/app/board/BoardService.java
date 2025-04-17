5f09b9d1b59a12895e3158abc82eeb1bee009136
woojin <pp09156@naver.com>
2025-04-17 17:43:56 +0900

    04-17
    board(list, detail, update, delete), front(login, join)

--- src/main/java/com/woojin/app/board/BoardService.java
+++ src/main/java/com/woojin/app/board/BoardService.java
@@ -2,11 +2,17 @@ package com.woojin.app.board;
 
 import java.util.List;
 
+import com.woojin.app.home.util.Pager;
+
 public interface BoardService {
 	
-	public List<BoardVO> getList() throws Exception;
+	public List<BoardVO> getList(Pager pager) throws Exception;
 	
 	public BoardVO getDetail(BoardVO boardVO) throws Exception;
 	
 	public int add(BoardVO boardVO) throws Exception;
+	
+	public int update(BoardVO boardVO) throws Exception;
+	
+	public int delete(BoardVO boardVO) throws Exception;
 }
